6c81a24dcc1b9f4be0409981a25b53fd57bd7ad8
Christian Curilla <C.Curilla@funk-gruppe.de>
2024-05-06 17:56:24 +0200

    Add project files.

---
+++ Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace SimpleKafkaSaslProducer;
+
+public class Options
+{
+    [Option(Required = true, HelpText = "Adresse und Port des Kafka Brokers.")]
+    public string? BootstrapServers { get; set; }
+
+    [Option(Required = true, HelpText = " Benutzername für die SASL-Authentifizierung.")]
+    public string? SaslUsername { get; set; }
+
+    [Option(Required = true, HelpText = "Passwort für die SASL-Authentifizierung.")]
+    public string? SaslPassword { get; set; }
+
+    [Option(Required = true, HelpText = "Topic, dem der Consumer beitreten soll.")]
+    public string Topic { get; set; } = string.Empty;
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using CommandLine;
+using Confluent.Kafka;
+
+namespace SimpleKafkaSaslProducer;
+
+internal class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(async opt =>
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Kafka: Verbindung wird beendet.");
+                // Bei einer Tastenunterbrechung (z.B. CTRL+C) die Cancellation durchführen
+                eventArgs.Cancel = true; // Verhindert, dass das Programm beendet wird
+                cancellationTokenSource.Cancel();
+            };
+
+            var configDictionary = new Dictionary<string, string>
+            {
+                { "bootstrap.servers", opt.BootstrapServers },
+                { "security.protocol", "SASL_SSL" },
+                { "sasl.mechanism", "PLAIN" },
+                { "sasl.username", opt.SaslUsername },
+                { "sasl.password", opt.SaslPassword },
+                { "enable.idempotence", "True" }
+            };
+
+            var config = new ProducerConfig(configDictionary);
+
+            var producer = new ProducerBuilder<string, byte[]>(config).Build();
+
+            try
+            {
+                Console.WriteLine("Bitte Key eingeben");
+                var key = Console.ReadLine();
+
+                Console.WriteLine("Bitte Value eingeben");
+                var value = Console.ReadLine();
+
+                var message = new Message<string, byte[]>
+                {
+                    Key = key,
+                    Value = Encoding.UTF8.GetBytes(value)
+                };
+
+                var report = await producer.ProduceAsync(opt.Topic, message, cancellationTokenSource.Token);
+                //{
+                Console.WriteLine($"Status: {report.Status}.");
+                //});
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(-1);
+            }
+        });
+        Console.WriteLine("Anwendung wird beendet. Bitte beliebigen Knopf drücken.");
+
+        Console.ReadKey();
+        return 0;
+    }
+}
